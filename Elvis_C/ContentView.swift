3bb286c9a9415b10d9f5eccd5ce980d62cbfcb4a
Titus <tistauffer@ascensionacademy.org>
2026-01-16 09:28:13 -0600

    Initial Commit

---
+++ Elvis_C/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elvis_C
+//
+//  Created by Titus Stauffer on 1/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elvis_C/Elvis_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elvis_CApp.swift
+//  Elvis_C
+//
+//  Created by Titus Stauffer on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Elvis_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
